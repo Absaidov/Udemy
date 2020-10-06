bbe8d5f8bd5c79cef293a805b471722fb3e88a68
JamaL <dabsaidov@bk.ru>
2020-10-06 22:44:59 +0300

    lesson 13
    lesson 13

--- js/script.js
+++ js/script.js
@@ -128,3 +128,32 @@ if (4 == 4) {
 } else {
     console.log("Error!"); //условия if ;
 }
+
+
+//Первый вариант кода условий if 
+// if (num < 49) {
+//     console.log("Error!");
+// } else if (num > 100) {
+//     console.log("Много!");
+// } else {
+//     console.log("OK!");
+// }
+
+// // Второй вариант кода условий
+// (num === 50) ? console.log("Ok!"): console.log("Error!");
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log("Неверно!");
+        break;
+    case 100:
+        console.log("Неверно!");
+        break;
+    case 50:
+        console.log(" В точку!");
+        break;
+    default:
+        console.log("Не в это раз! ");
+}
