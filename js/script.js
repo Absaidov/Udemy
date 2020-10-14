0deeb73be62eacc30f11a70459965c41050c7fa1
JamaL <dabsaidov@bk.ru>
2020-10-14 23:58:01 +0300

    lesson 19
    Callback function

--- js/script.js
+++ js/script.js
@@ -257,74 +257,100 @@
 // console.log(calc(15, 20));
 
 
-let numberOfFilms;
+// let numberOfFilms;
 
-function start(){
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-    while(numberOfFilms == "" || numberOfFilms == NaN || isNaN(numberOfFilms)){
-        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-    }
-};
-start();
+// function start(){
+//     numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+//     while(numberOfFilms == "" || numberOfFilms == NaN || isNaN(numberOfFilms)){
+//         numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+//     }
+// };
+// start();
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: true
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: true
 
-};
-function rememberMyFilms(){
-    for (let i = 0; i < 2; i++) {
-        const a = prompt("Один из последних просмотренных фильмов?", ""),
-            b = prompt("На сколько оцените его?", "");
+// };
+// function rememberMyFilms(){
+//     for (let i = 0; i < 2; i++) {
+//         const a = prompt("Один из последних просмотренных фильмов?", ""),
+//             b = prompt("На сколько оцените его?", "");
     
-            if(a != null && b != null && a != "" && b != "" &&  a.length < 50 && b.length < 50){
-            personalMovieDB.movies[a] = b;
-            console.log("DONE! ");
-            } else {
-                console.log("ERROR");
-                i--; 
-            }
-    } 
-};
-rememberMyFilms();
+//             if(a != null && b != null && a != "" && b != "" &&  a.length < 50 && b.length < 50){
+//             personalMovieDB.movies[a] = b;
+//             console.log("DONE! ");
+//             } else {
+//                 console.log("ERROR");
+//                 i--; 
+//             }
+//     } 
+// };
+// rememberMyFilms();
 
 
 
-function detectPersonalLevel() {
-    if(personalMovieDB.count < 10){
-        console.log("Просмотрено мало фильмов");
-    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30){
-        console.log("Среднее количество фильмов!");
-    } else if (personalMovieDB.count >= 30) {
-        console.log("Вы киноман");
-    } else {
-        console.log("ОШИБКА");
-    };
+// function detectPersonalLevel() {
+//     if(personalMovieDB.count < 10){
+//         console.log("Просмотрено мало фильмов");
+//     } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30){
+//         console.log("Среднее количество фильмов!");
+//     } else if (personalMovieDB.count >= 30) {
+//         console.log("Вы киноман");
+//     } else {
+//         console.log("ОШИБКА");
+//     };
     
-};
-detectPersonalLevel();
+// };
+// detectPersonalLevel();
 
 
-function showMyDB(hidden){
+// function showMyDB(hidden){
 
-    if(!hidden) {
-        console.log(personalMovieDB); 
-    }
-};
-showMyDB(personalMovieDB.privat); 
-
-
-function writeYoursGenres(){
-    for(let i = 1; i <= 3; i++){
-        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
-        personalMovieDB.genres[i - 1] = genre;
-    }
-};
-
-writeYoursGenres(); 
+//     if(!hidden) {
+//         console.log(personalMovieDB); 
+//     }
+// };
+// showMyDB(personalMovieDB.privat); 
 
 
+// function writeYoursGenres(){
+//     for(let i = 1; i <= 3; i++){
+//         const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+//         personalMovieDB.genres[i - 1] = genre;
+//     }
+// };
 
+// writeYoursGenres(); 
+
+
+function first(){
+    //do something
+    setTimeout(function(){
+
+        console.log(1);
+    }, 500);
+}
+
+function second(){
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback){
+    console.log(`Я учу: ${lang}`);
+    callback();
+}
+
+function done(){
+
+    console.log("Я прошел этот урок!");
+}
+
+
+learnJS("JavaScript", done);
