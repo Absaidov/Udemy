9bcc697153fd64695a9b805f2fe0e4858c8d95a0
JamaL <dabsaidov@bk.ru>
2020-10-16 00:35:21 +0300

    LESSON20
    Objects

--- js/script.js
+++ js/script.js
@@ -324,33 +324,62 @@
 //     }
 // };
 
-// writeYoursGenres(); 
+// // writeYoursGenres(); 
 
 
-function first(){
-    //do something
-    setTimeout(function(){
+// function first(){
+//     //do something
+//     setTimeout(function(){
 
-        console.log(1);
-    }, 500);
-}
+//         console.log(1);
+//     }, 500);
+// }
 
-function second(){
-    console.log(2);
-}
+// function second(){
+//     console.log(2);
+// }
 
-first();
-second();
+// first();
+// second();
 
-function learnJS(lang, callback){
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
+// function learnJS(lang, callback){
+//     console.log(`Я учу: ${lang}`);
+//     callback();
+// }
 
-function done(){
+// function done(){
 
-    console.log("Я прошел этот урок!");
-}
+//     console.log("Я прошел этот урок!");
+// }
 
 
-learnJS("JavaScript", done);
+// learnJS("JavaScript", done);
+
+
+
+
+
+ 
+// console.log(Object.keys(options).length);
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+// let counter = 0;
+// for(let key in options){
+//      if(typeof(options[key] === "object"))  {
+//         for (let i in options[key]){
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             counter++;
+
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+
+// }
+
+// console.log(counter);
